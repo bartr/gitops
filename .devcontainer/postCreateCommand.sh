6400621d0cf5fe65722dae529ca7c74ae51344a2
Bart Robertson <bartr@microsoft.com>
2024-11-12 16:35:49 +0000

    updated Codespaces

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,11 @@
 } >> "$HOME/.oh-my-zsh/custom/alias.zsh"
 
 echo "Creating k3d cluster"
-# k3d cluster create \
-# -p '80:80@loadbalancer' \
-# -p '443:443@loadbalancer' \
-# -p '8080:8080@loadbalancer'
+k3d cluster create \
+    --k3s-arg --disable=traefik@server:0 \
+    -p 30080:30080@server:0 \
+    -p 31080:31080@server:0 \
+    -p 32080:32080@server:0 \
+    -p 80:80@loadbalancer \
+    -p 443:443@loadbalancer \
+    -p 8080:8080@loadbalancer
